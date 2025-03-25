2cb008c121bec432af8050665e991c0304c9748e
EricShinozaki <emfshinozaki@gmail.com>
2025-03-24 22:03:39 -0700

    Add sign up screen and account creation via firebase

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key, required this.title});
@@ -11,16 +13,192 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  final emailController =  TextEditingController();
+  String errorMessage = "";
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Expanded(
+            flex: 20,
+            child:
+            Image(
+                image: NetworkImage(
+                    'https://cdn.pixabay.com/photo/2024/02/23/08/27/apple-8591539_1280.jpg'
+                ),
+                fit: BoxFit.cover
+            ),
+          ),
+          Expanded(
+            flex: 80,
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 15, bottom: 15),
+                  child: Text(
+                    'Create an account',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 35, right: 35, bottom: 10, top: 10),
+                  child: TextField(
+                    obscureText: false,
+                    controller:  emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      labelText: 'Email',
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 35, right: 35, top: 10, bottom: 10),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.password)
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 35, right: 35, top: 10, bottom: 10),
+                  child: TextField(
+                    obscureText: true,
+                    controller: confirmPasswordController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'Confirm Password',
+                        prefixIcon: Icon(Icons.password)
+                    ),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 35, right: 35, top: 20, bottom: 20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: Divider(
+                              thickness: 1,
+                              color: Colors.black,
+                            )
+                        ),
+                      ],
+                    )
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 35, right: 35),
+                    child: SizedBox(
+                        width: double.infinity,
+                        height: 56,
+                        child: FilledButton.tonal(
+                            onPressed: () async {
+                              String emailText = emailController.text;
+                              String passwordText = passwordController.text;
+                              String confirmPasswordText = confirmPasswordController.text;
+                              if(emailText.isEmpty || passwordText.isEmpty || confirmPasswordText.isEmpty){
+                                setState(() {
+                                  errorMessage = "Please fill out all fields";
+                                });
+                              } else if(passwordText != confirmPasswordText){
+                                setState(() {
+                                  errorMessage = "Password does not match";
+                                });
+                              } else {
+                                try {
+                                  final credential = await FirebaseAuth.instance
+                                      .createUserWithEmailAndPassword(
+                                    email: emailText,
+                                    password: passwordText,
+                                  );
+                                  setState(() {
+                                    errorMessage = ""; // Clear error on success
+                                  });
+                                } on FirebaseAuthException catch (e) {
+                                  setState(() {
+                                    if (e.code == 'weak-password') {
+                                      errorMessage =
+                                      'The password provided is too weak.';
+                                    } else
+                                    if (e.code == 'email-already-in-use') {
+                                      errorMessage =
+                                      'The account already exists for that email.';
+                                    }
+                                  });
+                                } catch (e) {
+                                  setState(() {
+                                    errorMessage = e.toString();
+                                  });
+                                }
+                              }
+
+                              if(errorMessage.isEmpty){
+                                errorMessage = "Successfully created account";
+                              }
+
+                              Future.delayed(Duration(seconds: 3), () {
+                                setState(() {
+                                  errorMessage = "";
+                                });
+                              });
+                            },
+                            style: ButtonStyle(
+                              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                    side: BorderSide(color: Colors.black),
+                                  )
+                              ),
+                              backgroundColor: WidgetStateProperty.all(Colors.lightBlueAccent),
+                            ),
+                            child: Text("Create Account")
+                        )
+                    )
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 35, right: 35, bottom: 10, top: 15),
+                  child: Text(errorMessage),
+                )
+              ],
+            ),
+          )
+          /*
+           FloatingActionButton(
+             onPressed: () {
+               showDialog(
+                 context: context,
+                 builder: (context) {
+                   return AlertDialog(
+                     content: Text("password: ${passwordController.text} email: ${emailController.text}"),
+                   );
+                 },
+               );
+             }
+           )
+           */
+        ],
       ),
     );
   }
