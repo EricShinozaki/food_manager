e8791b5e677a75929c6246b7711fa5a07974c67c
EricShinozaki <emfshinozaki@gmail.com>
2025-03-31 05:02:36 -0700

    Add item screen

--- lib/screens/add_item_screen.dart
+++ lib/screens/add_item_screen.dart
@@ -10,19 +10,119 @@ class AddItemScreen extends StatefulWidget {
 }
 
 class _AddItemScreenState extends State<AddItemScreen> {
+  final nameController = TextEditingController();
+  final quantityController = TextEditingController();
+  final unitController = TextEditingController();
+  final noteController = TextEditingController();
+  final nutritionController = TextEditingController();
+
+  List<String> nutritionData = [];
+
+  void addNutrition() {
+    final nutritionItem = nutritionController.text;
+
+    if (nutritionItem.isNotEmpty) {
+      setState(() {
+        nutritionData.add(nutritionItem);
+      });
+
+      nutritionController.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-    );
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+            child: Column(children: [
+          Column(children: [
+            Container(
+              margin: EdgeInsets.only(top: 15, bottom: 5, left: 20, right: 20),
+              child: TextField(
+                obscureText: false,
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  labelText: 'Item Name',
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5, bottom: 5, left: 20, right: 20),
+              child: TextField(
+                obscureText: false,
+                controller: quantityController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  labelText: 'Item Quantity',
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5, bottom: 5, left: 20, right: 20),
+              child: TextField(
+                obscureText: false,
+                controller: unitController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  labelText: 'Item Unit Placeholder',
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5, bottom: 5, left: 20, right: 20),
+              child: TextField(
+                obscureText: false,
+                controller: noteController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  labelText: 'Notes',
+                ),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 5, bottom: 5, left: 20, right: 20),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        obscureText: false,
+                        controller: nutritionController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          labelText: 'Nutrition',
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: addNutrition,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )),
+            SizedBox(height: 20),
+            Text("Added Nutrition Data:"),
+            ...nutritionData.map((data) => Text(data)),
+          ]),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text("tempButtonSection"),
+          )
+        ])));
   }
 }
-
